002454b55bff2552f6a892a832b79813f00d0300
albanybuipe96 <mohammedarahman96@gmail.com>
2023-01-11 00:39:47 +0000

    Added default colors for app

---
+++ app/src/main/java/com/nexus/quizler/ui/color/QuizlerColor.kt
@@ -0,0 +1,13 @@
+package com.nexus.quizler.ui.color
+
+import androidx.compose.ui.graphics.Color
+
+object QuizlerColor {
+
+    val mLightGray = Color(0xffb4b9cd)
+    val mOffWhite = Color(0xffeceeef)
+    val mLightPurple = Color(0xff545a75)
+    val mDarkPurple = Color(0xff262C49)
+    val mLightBlue = Color(0xff127EEA)
+
+}
